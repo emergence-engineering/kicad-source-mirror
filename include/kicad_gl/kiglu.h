d8e14c896f436bc3c3c30d2f34564d80f114469d
Istvan Matejcsok <119620946+matejcsok-ee@users.noreply.github.com>
2026-06-17 10:49:19 +0200

    fix: 🐛 openGL import

--- include/kicad_gl/kiglu.h
+++ include/kicad_gl/kiglu.h
@@ -33,7 +33,7 @@
 // System GL headers (assumed to be loaded via wxGLCanvas or other dependencies)
 #ifdef __EMSCRIPTEN__
 // For WASM, use kiglew.h which provides GL with our stubs and wrappers
-#include <gal/opengl/kiglew.h>
+#include <gal/webgl/kiglew.h>
 #elif defined( __APPLE__ )
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
